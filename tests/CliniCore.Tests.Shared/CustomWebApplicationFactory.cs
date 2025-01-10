f7d1dfc0d5b51e0bca293f0a42d7b8845466999e
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-10 19:46:01 +0200

    Add initial testing setup

---
+++ tests/CliniCore.Tests.Shared/CustomWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿using CliniCore.Modules.Appointments.Shell;
+using CliniCore.Modules.Availability.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CliniCore.Tests.Shared;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string databaseName = "fixed";
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.UseEnvironment("Test");
+
+        builder.ConfigureServices(services =>
+        {
+            // Remove existing DbContext registrations
+            RemoveDbContextRegistrations(services);
+
+            // Add all DbContexts with an in-memory database
+            AddInMemoryDbContexts(services);
+        });
+    }
+
+    private void RemoveDbContextRegistrations(IServiceCollection services)
+    {
+        var dbContextDescriptors = services
+            .Where(service => service.ServiceType.IsGenericType &&
+                              service.ServiceType.GetGenericTypeDefinition() == typeof(DbContextOptions<>))
+            .ToList();
+
+        foreach (var descriptor in dbContextDescriptors)
+        {
+            services.Remove(descriptor);
+        }
+    }
+
+    private void AddInMemoryDbContexts(IServiceCollection services)
+    {
+        services.AddDbContext<AvailabilityDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(databaseName);
+        });
+
+        services.AddDbContext<AppointmentsDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(databaseName);
+        });
+    }
+}
---
+++ tests/CliniCore.Tests.Shared/Utils/TestUtils.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CliniCore.Tests.Shared.Utils;
+
+public static class TestUtils
+{
+
+    /// <summary>
+    /// Clears and recreates the database for all DbContext types.
+    /// </summary>
+    /// <param name="factory">The web application factory.</param>
+    public static async Task ClearDatabaseAsync(CustomWebApplicationFactory factory)
+    {
+        // Get all types inheriting from DbContext
+        var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(DbContext).IsAssignableFrom(type)
+                           && !type.IsInterface
+                           && !type.IsAbstract);
+
+        using (var scope = factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+
+            foreach (var dbContextType in dbContextTypes)
+            {
+                // Resolve the DbContext from the scoped service provider
+                var dbContext = scopedServices.GetService(dbContextType) as DbContext;
+                if (dbContext is null) continue;
+
+                // Clear the database
+                await dbContext.Database.EnsureDeletedAsync();
+                // Recreate the database
+                await dbContext.Database.EnsureCreatedAsync();
+            }
+        }
+    }
+}
