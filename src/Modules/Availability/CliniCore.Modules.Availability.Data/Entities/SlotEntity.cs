c2fea7b38d943ed657e7cc0f4e109c3183d50ef0
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-10 23:50:37 +0200

    Add slot entity

---
+++ src/Modules/Availability/CliniCore.Modules.Availability.Data/Entities/SlotEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CliniCore.Modules.Availability.Data.Entities;
+public class SlotEntity
+{
+    public Guid Id { get; set; }
+    public DateTime Time { get; set; }
+    public Guid DoctorId { get; set; }
+    public string DoctorName { get; set; }
+    public bool IsReserved { get; set; }
+    public decimal Cost { get; set; }
+}
---
+++ src/Modules/Availability/CliniCore.Modules.Availability.Data/Migrations/20250110183604_AddSlotEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CliniCore.Modules.Availability.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSlotEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "availability");
+
+            migrationBuilder.CreateTable(
+                name: "Slots",
+                schema: "availability",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DoctorId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DoctorName = table.Column<string>(type: "TEXT", nullable: false),
+                    IsReserved = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Cost = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Slots",
+                schema: "availability");
+        }
+    }
+}
