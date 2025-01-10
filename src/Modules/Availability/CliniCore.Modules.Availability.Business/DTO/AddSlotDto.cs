c5cd27c709ebf58278b436bc2e57333cb4e0ce78
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-10 23:55:53 +0200

    Implement slots mapper

---
+++ src/Modules/Availability/CliniCore.Modules.Availability.Business/DTO/AddSlotDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CliniCore.Modules.Availability.Business.DTO;
+public class AddSlotDto
+{
+    public DateTime Time { get; set; }
+
+    public decimal Cost { get; set; }
+}
---
+++ src/Modules/Availability/CliniCore.Modules.Availability.Shared/DTO/SlotDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CliniCore.Modules.Availability.Shared.DTO;
+public class SlotDto
+{
+    public Guid Id { get; set; }
+    public DateTime Time { get; set; }
+    public Guid DoctorId { get; set; }
+    public string DoctorName { get; set; }
+    public bool IsReserved { get; set; }
+    public decimal Cost { get; set; }
+}
