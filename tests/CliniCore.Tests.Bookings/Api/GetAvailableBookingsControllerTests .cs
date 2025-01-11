a1df9797d4bdaf8651fe1b314f4f67e7088d52fd
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-11 19:10:39 +0200

    Add book appointment tests

--- tests/CliniCore.Tests.Bookings/Api/GetAvailableBookingsControllerTests .cs
+++ tests/CliniCore.Tests.Bookings/Api/GetAvailableBookingsControllerTests .cs
@@ -3,45 +3,54 @@ using CliniCore.Modules.Availability.Shared;
 using CliniCore.Modules.Availability.Shared.DTO;
 using CliniCore.Modules.Bookings.Application.GetAvailableBookings;
 using CliniCore.Tests.Shared;
+using CliniCore.Tests.Shared.Utils;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
 
 namespace CliniCore.Tests.Bookings.Api;
-public class GetAvailableBookingsControllerTests : IClassFixture<CustomWebApplicationFactory>
+public class GetAvailableBookingsControllerTests : IClassFixture<CustomWebApplicationFactory>, IAsyncLifetime
 {
     private readonly CustomWebApplicationFactory _factory;
     private readonly IFixture _fixture;
+    private readonly Mock<IAvailabilityModuleApi> _availabilityModuleApiMock;
+    private readonly HttpClient _httpClient;
+
 
     public GetAvailableBookingsControllerTests(CustomWebApplicationFactory factory)
     {
         _factory = factory;
         _fixture = new Fixture();
+        _availabilityModuleApiMock = new Mock<IAvailabilityModuleApi>();
+        _httpClient = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(_ => _availabilityModuleApiMock.Object);
+            });
+        }).CreateClient();
     }
+    public async Task InitializeAsync()
+    {
+        await TestUtils.ClearDatabaseAsync(_factory);
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
 
     [Fact]
     public async Task GetAvailableBookings_ShouldReturnOkWithAvailableBookings()
     {
         // Arrange
         var availableSlots = _fixture.CreateMany<SlotDto>(5).ToList();
-        var mockAvailabilityApi = new Mock<IAvailabilityModuleApi>();
-        mockAvailabilityApi
+        _availabilityModuleApiMock
             .Setup(api => api.GetAvailableSlotsAsync())
             .ReturnsAsync(availableSlots);
 
-        var client = _factory.WithWebHostBuilder(builder =>
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddScoped(_ => mockAvailabilityApi.Object);
-            });
-        }).CreateClient();
-
-
         // Act
-        var response = await client.GetAsync("/api/v1/AvailableBookings");
+        var response = await _httpClient.GetAsync("/api/v1/Bookings/Available");
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -54,21 +63,12 @@ public class GetAvailableBookingsControllerTests : IClassFixture<CustomWebApplic
     public async Task GetAvailableBookings_ShouldReturnInternalServerError_WhenExternalCallFails()
     {
         // Arrange
-        var mockAvailabilityApi = new Mock<IAvailabilityModuleApi>();
-        mockAvailabilityApi
+        _availabilityModuleApiMock
             .Setup(api => api.GetAvailableSlotsAsync())
             .ThrowsAsync(new Exception("Service unavailable"));
 
-        var client = _factory.WithWebHostBuilder(builder =>
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddScoped(_ => mockAvailabilityApi.Object);
-            });
-        }).CreateClient();
-
         // Act
-        var response = await client.GetAsync("/api/v1/AvailableBookings");
+        var response = await _httpClient.GetAsync("/api/v1/Bookings/Available");
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
