2656421953e68d5db090905b8ce8d00c740f8af4
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-13 22:52:29 +0200

    Refactor migrations folder structure

---
+++ src/Modules/Bookings/CliniCore.Modules.Bookings.Infrastructure/DAL/Migrations/20250113204949_AddBookingEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CliniCore.Modules.Bookings.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "booking");
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                schema: "booking",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SlotId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PatientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PatientName = table.Column<string>(type: "TEXT", nullable: false),
+                    ReservedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_SlotId",
+                schema: "booking",
+                table: "Bookings",
+                column: "SlotId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings",
+                schema: "booking");
+        }
+    }
+}
