ecff47407b74085d0110c33c0d5d6f3740585d48
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-14 21:40:49 +0200

    Add docs for appointments module api endpoints

--- src/Modules/Appointments/CliniCore.Modules.Appointments.Shell/Controllers/AppointmentsController.cs
+++ src/Modules/Appointments/CliniCore.Modules.Appointments.Shell/Controllers/AppointmentsController.cs
@@ -3,6 +3,8 @@ using CliniCore.Modules.Appointments.Core.Exceptions;
 using CliniCore.Modules.Appointments.Core.InputPorts;
 using CliniCore.Modules.Appointments.Core.Models;
 using CliniCore.Modules.Appointments.Shell.DTO;
+using CliniCore.Shared.Exceptions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CliniCore.Modules.Appointments.Shell.Controllers;
@@ -18,14 +20,38 @@ public class AppointmentsController : ControllerBase
         _appointmentService = appointmentService;
     }
 
+    /// <summary>
+    /// Get a list of upcoming appointments.
+    /// </summary>
+    /// <remarks>
+    /// This endpoint retrieves all appointments that are scheduled for the future. The response includes details about each appointment.
+    /// </remarks>
+    /// <returns>A list of upcoming appointments.</returns>
+    /// <response code="200">Returns the list of upcoming appointments.</response>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<AppointmentDto>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetUpcomingAppointments()
     {
         var result = await _appointmentService.GetUpcomingAppointmentsAsync();
         return Ok(result);
     }
 
+    /// <summary>
+    /// Update the status of an appointment.
+    /// </summary>
+    /// <remarks>
+    /// Allows updating the status of an appointment (to "Completed" or "Cancelled").
+    /// </remarks>
+    /// <param name="id">The ID of the appointment to update.</param>
+    /// <param name="request">The new status for the appointment.</param>
+    /// <returns>A confirmation message indicating the status update.</returns>
+    /// <response code="200">Status updated successfully.</response>
+    /// <response code="400">If the status is invalid.</response>
+    /// <response code="404">If the appointment ID does not exist.</response>
     [HttpPatch("{id:guid:required}")]
+    [ProducesResponseType(typeof(UpdateStatusResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateAppointmentStatus(Guid id, [FromBody] UpdateStatusRequest request)
     {
         if (!Enum.TryParse<AppointmentStatus>(request.Status, out var status))
