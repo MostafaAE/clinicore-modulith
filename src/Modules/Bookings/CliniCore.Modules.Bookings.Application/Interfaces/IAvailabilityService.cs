fb338f2d537c3c1678b204f5c13bf4d8c50c2d4e
MostafaAE <mostafa.aymanae@gmail.com>
2025-01-27 22:22:11 +0200

    Implement availability module api wrapper in bookings infrastructure layer

---
+++ src/Modules/Bookings/CliniCore.Modules.Bookings.Application/Interfaces/IAvailabilityService.cs
@@ -0,0 +1,8 @@
+﻿using CliniCore.Modules.Bookings.Application.GetAvailableBookings;
+
+namespace CliniCore.Modules.Bookings.Application.Interfaces;
+public interface IAvailabilityService
+{
+    Task<IEnumerable<AvailableBookingDto>> GetAvailableSlotsAsync();
+    Task<AvailableBookingDto> GetSlotByIdAsync(Guid id);
+}
